62392f173951ff2845af10a30a3b7a10730e0b59
Sanjeev Kumar <sanjeev2856@gmail.com>
2026-07-19 14:39:53 +0530

    Fixed dialog open close direction

--- client/src/shared/ui/dialog.tsx
+++ client/src/shared/ui/dialog.tsx
@@ -40,8 +40,6 @@ function DialogContent({
           "data-[state=open]:animate-in data-[state=closed]:animate-out",
           "data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
           "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-          "data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
-          "data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
           "duration-200",
           className
         )}
